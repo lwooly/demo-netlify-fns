caa4316273d305b666430e14f4edd5cc6bbccc74
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2023-12-05 11:54:54 +0000

    add serverless car api

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,32 @@
 const messageNode = document.getElementById('message')
 
-const render = (message = "", node = messageNode) => {
-messageNode.textContent = message
+
+const render = (cars = [], DOMnode = messageNode) => {
+    console.log(cars, `cars`)
+    const frag = document.createDocumentFragment();
+    cars.forEach(car => {
+        const li = document.createElement('li');
+        li.innerHTML = `<img src="${car.avatar_url}" /><span>${car.name} (${car.bhp})</span>`
+        frag.append(li)
+    })
+    DOMnode.append(frag)
 }
+
 
 const getData = async () => {
     try {
-        const res = await fetch('./.netlify/functions/hello-world')
+        const res = await fetch('/.netlify/functions/read-all-cars')
 
         if (!res.ok) {
             throw res
         }
-
         const data = await res.json()
-        render(data.message)
-    } catch (error) {
         
+        render(data)
+        
+    } catch (error) {
+        alert('Error fetching, check console!')
+        console.log(error)
     }
 }
 
